a4acd017aa347d9bfe28979d5f07cdd807fc75f1
Krzysztof Rodzaj <krzysztof.rodzaj@mobica.com>
2023-10-16 09:14:49 +0200

    Changed windows script from .bat to powershell

--- tests/windows_env.bat
+++
@@ -1,4 +0,0 @@
-curl https://www.autoitscript.com/files/autoit3/autoit-v3.zip --output %USERPROFILE%\Downloads\autoit-v3.zip
-cd %USERPROFILE%\Downloads
-tar -xf autoit-v3.zip
-setx path "%PATH%;%cd%\install"
